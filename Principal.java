c60dfe8febe9b2883b5ac2ac147f187308e2fd06
GuillermicoM <158084532+GuillermicoM@users.noreply.github.com>
2024-02-06 19:42:08 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -2,4 +2,8 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
+    public void mostrarfechayhora() {
+        Fecha fechaHoraActual = new fecha();
+        System.out.println("Fecha y hora actual: " + fechaHoraActual);
+    }
 }
